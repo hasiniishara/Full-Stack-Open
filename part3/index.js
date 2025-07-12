a672702a2111c6a624273058608fc1dd1f9628e3
hasiniishara <hasiniisharao4@gmail.com>
2025-07-12 13:30:01 +0300

    impemented mongodb connection exerices

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(express.json())
 app.use(morgan('tiny'))
@@ -15,31 +17,9 @@ morgan.token('body', (req) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.post('/api/persons', (request, response)  =>{
     const body = request.body
+    console.log(body)
     
     if (!body.name || !body.number) {
         return response.status(400).json({
@@ -47,70 +27,64 @@ app.post('/api/persons', (request, response)  =>{
         })
     }
 
-    const nameExists = persons.some(person => person.name === body.name)
-    if (nameExists) {
-        return response.status(400).json({
-        error: 'name must be unique'
-        })
-    }
+    const person = new Person({
+      name: body.name,
+      number: body.number || false,
+    })
 
-    const newPerson = {
-        id: Math.floor(Math.random() * 1000000),
-        name: body.name,
-        number: body.number
-    }
-    response.json(newPerson)
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
 })
 
 app.get('/api/persons', (request, response)  =>{
-    response.json(persons)
+    Person.find({}).then(result => {
+      result.forEach(person => {
+        console.log(person)
+      })
+    })
 })
 
-app.get('/info', (request, response) =>{
-    const numOfPersons = persons.length
-    const date = new Date()
+// app.get('/info', (request, response) =>{
+//     const numOfPersons = persons.length
+//     const date = new Date()
     
-    const html = `
-        <div>
-            <p>Phonebook has info for ${numOfPersons} people</p>
-            <p>${date}</p>
-        </div>
-    `
-    response.send(html)
-})
+//     const html = `
+//         <div>
+//             <p>Phonebook has info for ${numOfPersons} people</p>
+//             <p>${date}</p>
+//         </div>
+//     `
+//     response.send(html)
+// })
 
 app.get('/api/persons/:id', (request, response)  =>{
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-  
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+      Person.findById(request.params.id).then(note => {
+        response.json(note)
+    })
 })
 
-app.delete('/api/persons/:id', (request, response)  =>{
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
-})
+// app.delete('/api/persons/:id', (request, response)  =>{
+//     const id = request.params.id
+//     persons = persons.filter(person => person.id !== id)
+//     response.status(204).end()
+// })
 
-app.put('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const body = request.body
+// app.put('/api/persons/:id', (request, response) => {
+//   const id = request.params.id
+//   const body = request.body
 
-  const index = persons.findIndex(p => p.id === id)
-  if (index === -1) {
-    return response.status(404).json({ error: 'Person not found' })
-  }
+//   const index = persons.findIndex(p => p.id === id)
+//   if (index === -1) {
+//     return response.status(404).json({ error: 'Person not found' })
+//   }
 
-  const updatedPerson = { ...persons[index], number: body.number }
-  persons[index] = updatedPerson
-  response.json(updatedPerson)
-})
+//   const updatedPerson = { ...persons[index], number: body.number }
+//   persons[index] = updatedPerson
+//   response.json(updatedPerson)
+// })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
