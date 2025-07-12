1de7a1a9f32cfb39e36483f81991f966f2fd8a5f
hasiniishara <hasiniisharao4@gmail.com>
2025-07-12 14:13:16 +0300

    impemented mongodb connection exerices

--- part3/index.js
+++ part3/index.js
@@ -17,7 +17,7 @@ morgan.token('body', (req) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
-app.post('/api/persons', (request, response)  =>{
+app.post('/api/persons', (request, response, next)  =>{
     const body = request.body
     console.log(body)
     
@@ -35,52 +35,81 @@ app.post('/api/persons', (request, response)  =>{
     person.save().then(savedPerson => {
       response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons', (request, response)  =>{
+app.get('/api/persons', (request, response, next)  =>{
     Person.find({}).then(result => {
       response.json(result)
     })
+    .catch(error => next(error))
 })
 
-// app.get('/info', (request, response) =>{
-//     const numOfPersons = persons.length
-//     const date = new Date()
+app.get('/info', (request, response) =>{
+    const numOfPersons = persons.length
+    const date = new Date()
     
-//     const html = `
-//         <div>
-//             <p>Phonebook has info for ${numOfPersons} people</p>
-//             <p>${date}</p>
-//         </div>
-//     `
-//     response.send(html)
-// })
-
-app.get('/api/persons/:id', (request, response)  =>{
-      Person.findById(request.params.id).then(note => {
-        response.json(note)
-    })
+    const html = `
+        <div>
+            <p>Phonebook has info for ${numOfPersons} people</p>
+            <p>${date}</p>
+        </div>
+    `
+    response.send(html)
 })
 
-// app.delete('/api/persons/:id', (request, response)  =>{
-//     const id = request.params.id
-//     persons = persons.filter(person => person.id !== id)
-//     response.status(204).end()
-// })
+app.get('/api/persons/:id', (request, response, next)  =>{
+      Person.findById(request.params.id).then(person => {
+        if (note) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }  
+    })
+    .catch(error => next(error))
+})
 
-// app.put('/api/persons/:id', (request, response) => {
-//   const id = request.params.id
-//   const body = request.body
+app.delete('/api/persons/:id', (request, response, next)  =>{
+    Person.findByIdAndDelete(request.params.id)
+      .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
-//   const index = persons.findIndex(p => p.id === id)
-//   if (index === -1) {
-//     return response.status(404).json({ error: 'Person not found' })
-//   }
+app.put('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  const body = request.body
 
-//   const updatedPerson = { ...persons[index], number: body.number }
-//   persons[index] = updatedPerson
-//   response.json(updatedPerson)
-// })
+  const { name, number } = request.body
+
+  Note.findById(id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      person.name = name
+      person.number = number
+
+      return person.save().then((updatedPerson) => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
