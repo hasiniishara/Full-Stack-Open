50ddaa3d2fd5a4d5bbe3b686824756c714ffb931
hasiniishara <hasiniisharao4@gmail.com>
2025-07-26 14:02:16 +0300

    Completed Exercises 4.8.-4.12

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,119 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
+
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+  console.log('Response body:', response.body)
+  assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('blog posts have id field instead of _id', async () => {
+  const response = await api.get('/api/blogs')
+  const blog = response.body[0]
+
+  assert.ok(blog.id, 'Expected blog to have an id field')
+  assert.strictEqual(blog._id, undefined, 'Expected blog not to have _id field')
+})
+
+test('a valid blog can be added ', async () => {
+  const newBlog = {
+    _id: "5a422aa71b54a676234d17f9",
+    title: "Go To",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const title = response.body.map(r => r.title)
+
+  assert.strictEqual(response.body.length, initialBlogs.length + 1)
+
+  assert(title.includes('Go To'))
+})
+test('if likes is missing, it defaults to 0', async () => {
+  const newBlog = {
+    title: 'New Blog Without Likes',
+    author: 'No Likes Author',
+    url: 'http://nolikes.com'
+  }
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const savedBlog = response.body
+  assert.strictEqual(savedBlog.likes, 0)
+})
+
+test('blog without url or title is not added', async () => {
+  const newBlog = {
+    _id: "5a422aa71b54a676234d17f9",
+    author: "Edsger W. Dijkstra",
+    likes: 5,
+    __v: 0
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
